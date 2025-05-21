663200e141bc6ec5ba57ce3e874892299321f394
KunnuSherry <gamingvenom465@gmail.com>
2025-05-21 12:38:34 +0530

    01_JobPortalWebsite - Made the backend of application, Now 90 percent of the backend is done

--- 01_JobPortalWebsite/backend/index.js
+++ 01_JobPortalWebsite/backend/index.js
@@ -4,9 +4,13 @@ import cors from "cors"
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./utils/db.js";
+
+//Importing routes
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js"
+import applicationRoute from "./routes/application.route.js"
+import { Application } from "./models/applicationModel.js";
 
 
 const app = express();
@@ -36,6 +40,7 @@ app.use("/api/v1/user", userRoute)
 //eg: http://localhost:8000/api/v1/user/register
 app.use("/api/v1/company", companyRoute)
 app.use("/api/v1/job", jobRoute)
+app.use("/api/v1/application", applicationRoute)
 
 app.listen(port, ()=>{
     connectDB();
